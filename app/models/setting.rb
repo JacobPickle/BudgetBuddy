61f0256510fbe46b3d407c93bc390dcb28e9e26f
JacobPickle <119011795+JacobPickle@users.noreply.github.com>
2023-09-11 02:36:57 -0500

    Added weekly budget and summations

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,4 +2,5 @@
 # https://github.com/huacnlee/rails-settings-cached
 class Setting < RailsSettings::Base
   field :weeks, type: :integer, default: 2
+  field :weekly_budget, type: :integer, default: 150
 end
