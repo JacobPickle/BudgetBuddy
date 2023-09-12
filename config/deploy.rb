553f0dcb4f3530f15c96ec18c1602ab8153777a0
JacobPickle <119011795+JacobPickle@users.noreply.github.com>
2023-09-12 15:06:23 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,9 +15,6 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind,       "unix:///home/deploy/apps/BudgetBuddy/shared/tmp/sockets/puma.sock" 
 set :puma_state,      "/home/deploy/apps/BudgetBuddy/shared/tmp/pids/puma.state" 
 set :puma_pid,        "/home/deploy/apps/BudgetBuddy/shared/tmp/pids/puma.pid" 
