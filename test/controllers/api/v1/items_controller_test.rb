cd9c21c50961c0b23b5d0a9b7b8c173e2d7d1d4d
JacobPickle <119011795+JacobPickle@users.noreply.github.com>
2023-10-02 16:43:05 -0500

    Very basic test that provide coverage

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -1,23 +1,47 @@
 require "test_helper"
 
 class Api::V1::ItemsControllerTest < ActionDispatch::IntegrationTest
+  setup do 
+    @storeHyvee = Store.create({name:"Hyvee"})
+
+    @purchase = Purchase.create({purchase_date:Date.current, total:5.99, store:@storeHyvee})
+
+    @item = Item.create({name:"hotdogs", price:2.99, purchase:@purchase})
+  end
+
   test "should get index" do
     get api_v1_items_index_url
     assert_response :success
   end
 
   test "should get create" do
-    get api_v1_items_create_url
+    post api_v1_items_create_url, params: {name: "testName", price: 1.67, purchase_id: @purchase.id}
+    assert_response :success
+  end
+
+  test "should error create" do
+    post api_v1_items_create_url, params: {price: 1.67, purchase_id: @purchase.id}
     assert_response :success
   end
 
   test "should get show" do
-    get '/items/show/0'
+    get "/api/v1/items/show/#{@item.id}"
+    item = @response.parsed_body
+    assert_equal 2.99, item['price']
+    assert_equal "hotdogs", item['name']
     assert_response :success
   end
 
-  test "should get destroy" do
-    get '/items/destroy/0'
+  test "should get show by purchase id" do
+    get "/api/v1/items/show_by_purchase/#{@purchase.id}"
+    item = @response.parsed_body
+    assert_equal 2.99, item[0]['price']
+    assert_equal "hotdogs", item[0]['name']
+    assert_response :success
+  end
+
+  test "should delete destroy" do
+    delete "/api/v1/items/destroy/#{@item.id}"
     assert_response :success
   end
 end
--- test/controllers/api/v1/stores_controller_test.rb
+++ test/controllers/api/v1/stores_controller_test.rb
@@ -1,23 +1,29 @@
 require "test_helper"
 
 class Api::V1::StoresControllerTest < ActionDispatch::IntegrationTest
+  setup do 
+    @storeHyvee = Store.create({name:"Hyvee"})
+  end
+
   test "should get index" do
     get api_v1_stores_index_url
     assert_response :success
   end
 
   test "should get create" do
-    get api_v1_stores_create_url
+    post api_v1_stores_create_url, params: {name: "Aldi"}
     assert_response :success
   end
 
   test "should get show" do
-    get '/stores/show/0'
+    get "/api/v1/stores/show/#{@storeHyvee.id}"
+    store = @response.parsed_body
+    assert_equal "Hyvee", store['name']
     assert_response :success
   end
 
   test "should get destroy" do
-    get '/stores/destroy/0'
+    delete "/api/v1/stores/destroy/#{@storeHyvee.id}"
     assert_response :success
   end
 end
